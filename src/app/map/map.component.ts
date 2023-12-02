54c0f55ac6645c50ee1453cd86edd3319fb2bf11
Dharam Dhameliya <dharam.dhameliya27@gmail.com>
2023-12-02 03:56:24 +0100

    Ex. 4 done

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,6 +6,7 @@ import { fromLonLat, toLonLat } from 'ol/proj';
 import View from 'ol/View';
 import Overlay, { Options as OverlayOptions } from 'ol/Overlay';
 import { toStringHDMS } from 'ol/coordinate';
+import XYZ from 'ol/source/XYZ';
 
 @Component({
   selector: 'app-map',
@@ -15,15 +16,19 @@ import { toStringHDMS } from 'ol/coordinate';
   styleUrl: './map.component.scss'
 })
 export class MapComponent implements OnInit {
-
   map: Map = new Map;
   popup: Overlay = new Overlay({});
+
+  wmsLayer!: TileLayer<XYZ>;
+
+  
 
   ngOnInit(): void {
     this.initMap(); // Initialize the map
     this.addClickListener(); // Add click listener to the map
   }
 
+  // Map initialization
   initMap(): void {
     this.map = new Map({
       target: 'map',
@@ -38,8 +43,8 @@ export class MapComponent implements OnInit {
       })
     });
 
-
-    this.popup = new Overlay({ // Create a new overlay
+    // Create a new overlay to display the popup
+    this.popup = new Overlay({
       element: document.getElementById('popup')!,
       autoPan: true,
       autoPanAnimation: {
@@ -47,7 +52,8 @@ export class MapComponent implements OnInit {
       }
     } as OverlayOptions);
 
-    this.map.addOverlay(this.popup); // Add the overlay to the map
+    // Add the overlay to the map
+    this.map.addOverlay(this.popup);
     const popupClose = document.getElementById('popupClose');
     if (popupClose) {
       popupClose.onclick = () => {
@@ -58,36 +64,47 @@ export class MapComponent implements OnInit {
       throw new Error('popupClose element not found');
     }
 
+    // Open Weather Map Layer
+    this.wmsLayer = new TileLayer({
+      source: new XYZ({
+        url: 'https://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?appid=f8d5a3b4b35fdb8afca577abbbfc54c7', // TA2(Air temperature at a height of 2 meters) layer
+              }),
+      visible: false, // Default visibility
+    });
+    this.map.addLayer(this.wmsLayer); // Add the layer to the map   
   }
 
-  addClickListener(): void { // Event listener to the map and display popup on click
+  // Event listener to the map and display popup on click
+  addClickListener(): void {
     this.map.on('singleclick', async (event) => {
       const coordinate = toLonLat(event.coordinate) as [number, number]; // Convert coordinate to EPSG-4326
       // console.log('Coordinate:', coordinate);  Debugging log for console
       const address = await this.fetchLocationInfo(coordinate); // Fetch address using coordinates
-      this.showPopup(coordinate, address || `Coordinates: ${toStringHDMS(coordinate)}`);                                                                                                                                                                                                                                                       
+      this.showPopup(coordinate, address || `Coordinates: ${toStringHDMS(coordinate)}`);
     });
   }
 
-  showPopup(coordinate: [number, number], content: string): void { // Display popup
+  // Display popup with address
+  showPopup(coordinate: [number, number], content: string): void {
     const element = document.getElementById('popup-content');
     if (element) {
       element.innerHTML = content;
-      this.popup.setPosition(fromLonLat(coordinate));
+      this.popup.setPosition(fromLonLat(coordinate)); // Set popup position
     } else {
       console.error('Popup content element not found');
     }
   }
 
-  async fetchLocationInfo(coordinate: [number, number]): Promise<string> { // Fetch address using coordinates
-    const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${coordinate[1]}&lon=${coordinate[0]}`; // Reverse Geocoding API
+  // Fetch address using coordinates
+  async fetchLocationInfo(coordinate: [number, number]): Promise<string> {
+    const reverseUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${coordinate[1]}&lon=${coordinate[0]}`; // Reverse Geocoding API
 
     try {
-      const response = await fetch(url);
+      const response = await fetch(reverseUrl);
       const data = await response.json();
       // console.log('Location Info:', data); // Debugging log for console
       const addressParts = [];
-      if (data.address.road) addressParts.push(data.address.road); 
+      if (data.address.road) addressParts.push(data.address.road);
       if (data.address.house_number) addressParts.push(data.address.house_number);
       if (data.address.city) addressParts.push(data.address.city);
       if (data.address.postcode) addressParts.push(data.address.postcode);
@@ -100,4 +117,9 @@ export class MapComponent implements OnInit {
     }
   }
 
+  // Toggle layer visibility on button click
+  toggleWmsLayer(): void {
+    const isVisible = this.wmsLayer.getVisible();
+    this.wmsLayer.setVisible(!isVisible);
+  }
 }

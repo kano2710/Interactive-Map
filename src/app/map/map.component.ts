57fe3557addffe530953a3981fc48bad81e39450
Dharam Dhameliya <dharam.dhameliya27@gmail.com>
2023-12-04 03:35:31 +0100

    Ex.3 Done

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,25 +7,49 @@ import View from 'ol/View';
 import Overlay, { Options as OverlayOptions } from 'ol/Overlay';
 import { toStringHDMS } from 'ol/coordinate';
 import XYZ from 'ol/source/XYZ';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import GeoJSON from 'ol/format/GeoJSON';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { Geometry } from 'ol/geom';
+import { Feature } from 'ol';
+import Draw from 'ol/interaction/Draw';
+import { NgIf } from '@angular/common';
+import Style from 'ol/style/Style';
+import Stroke from 'ol/style/Stroke';
+import Fill from 'ol/style/Fill';
+import CircleStyle from 'ol/style/Circle';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, NgIf],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
+
 export class MapComponent implements OnInit {
+
   map: Map = new Map;
   popup: Overlay = new Overlay({});
-
   wmsLayer!: TileLayer<XYZ>;
+  germanyGeoJsonLayer?: VectorLayer<VectorSource<Feature<Geometry>>>;
+  indiaGeoJsonLayer?: VectorLayer<VectorSource<Feature<Geometry>>>;
+  drawingLayer?: VectorLayer<VectorSource<Feature<Geometry>>>;
+  isDrawing: boolean = false;
 
+  // Styles for drawing features
+  pointStyle = new Style({ image: new CircleStyle({ radius: 5, fill: new Fill({ color: 'red' }), stroke: new Stroke({ color: 'black', width: 1 }) }) });
+  lineStyle = new Style({ stroke: new Stroke({ color: 'green', width: 2 }) });
+  polygonStyle = new Style({ stroke: new Stroke({ color: 'blue', width: 2 }), fill: new Fill({ color: 'rgba(0, 0, 255, 0.5)' }) });
+  circleStyle = new Style({ stroke: new Stroke({ color: 'yellow', width: 2 }), fill: new Fill({ color: 'rgba(255, 255, 0, 0.5)' }) });
   
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.initMap(); // Initialize the map
-    this.addClickListener(); // Add click listener to the map
+    this.addMapClickListener(); // Add click listener to the map
+    this.loadGeoJson(); // Load GeoJSON data
   }
 
   // Map initialization
@@ -48,13 +72,13 @@ export class MapComponent implements OnInit {
       element: document.getElementById('popup')!,
       autoPan: true,
       autoPanAnimation: {
-        duration: 250 // milliseconds
+        duration: 250 // in milliseconds
       }
     } as OverlayOptions);
 
     // Add the overlay to the map
     this.map.addOverlay(this.popup);
-    const popupClose = document.getElementById('popupClose');
+    const popupClose = document.getElementById('popupClose'); // Get the close button element
     if (popupClose) {
       popupClose.onclick = () => {
         this.popup.setPosition(undefined);
@@ -68,17 +92,40 @@ export class MapComponent implements OnInit {
     this.wmsLayer = new TileLayer({
       source: new XYZ({
         url: 'https://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?appid=f8d5a3b4b35fdb8afca577abbbfc54c7', // TA2(Air temperature at a height of 2 meters) layer
-              }),
-      visible: false, // Default visibility
+      }),
+      visible: false, // Default visibility (currently hidden)
     });
-    this.map.addLayer(this.wmsLayer); // Add the layer to the map   
+    this.map.addLayer(this.wmsLayer);
+
+    // Drawing layer
+    this.drawingLayer = new VectorLayer({
+      source: new VectorSource(),
+      style: (feature) => {
+        switch (feature.getGeometry()?.getType()) {
+          case 'Point':
+            return this.pointStyle;
+          case 'LineString':
+            return this.lineStyle;
+          case 'Polygon':
+            return this.polygonStyle;
+          case 'Circle':
+            return this.circleStyle;
+          default:
+            return undefined;
+        }
+      }
+    });
+    this.map.addLayer(this.drawingLayer);
   }
 
   // Event listener to the map and display popup on click
-  addClickListener(): void {
+  addMapClickListener(): void {
     this.map.on('singleclick', async (event) => {
-      const coordinate = toLonLat(event.coordinate) as [number, number]; // Convert coordinate to EPSG-4326
-      // console.log('Coordinate:', coordinate);  Debugging log for console
+      if (this.isDrawing) { // Don't show popup when drawing
+        return;
+      }
+
+      const coordinate = toLonLat(event.coordinate) as [number, number]; // Convert to lonlat
       const address = await this.fetchLocationInfo(coordinate); // Fetch address using coordinates
       this.showPopup(coordinate, address || `Coordinates: ${toStringHDMS(coordinate)}`);
     });
@@ -98,18 +145,16 @@ export class MapComponent implements OnInit {
   // Fetch address using coordinates
   async fetchLocationInfo(coordinate: [number, number]): Promise<string> {
     const reverseUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${coordinate[1]}&lon=${coordinate[0]}`; // Reverse Geocoding API
-
     try {
       const response = await fetch(reverseUrl);
       const data = await response.json();
       // console.log('Location Info:', data); // Debugging log for console
       const addressParts = [];
-      if (data.address.road) addressParts.push(data.address.road);
-      if (data.address.house_number) addressParts.push(data.address.house_number);
-      if (data.address.city) addressParts.push(data.address.city);
-      if (data.address.postcode) addressParts.push(data.address.postcode);
-      if (data.address.country) addressParts.push(data.address.country);
-
+      if (data.address.road) addressParts.push(data.address.road);  // Street name
+      if (data.address.house_number) addressParts.push(data.address.house_number); // House number
+      if (data.address.city) addressParts.push(data.address.city); // City
+      if (data.address.postcode) addressParts.push(data.address.postcode); // Postal code
+      if (data.address.country) addressParts.push(data.address.country); // Country
       return addressParts.join(', ');
     } catch (error) {
       console.error('Error fetching location info:', error);
@@ -117,9 +162,128 @@ export class MapComponent implements OnInit {
     }
   }
 
-  // Toggle layer visibility on button click
+  // Temperature layer visibility on button click
   toggleWmsLayer(): void {
     const isVisible = this.wmsLayer.getVisible();
     this.wmsLayer.setVisible(!isVisible);
+  }
+
+  // Load GeoJSON data
+  loadGeoJson(): void {
+    this.loadGermanyGeoJson();
+    this.loadIndiaGeoJson();
+  }
+
+  // Create or update GeoJSON layer
+  createOrUpdateLayer(geoJsonData: any, layerProperty: 'germanyGeoJsonLayer' | 'indiaGeoJsonLayer'): void {
+    const features = (new GeoJSON().readFeatures(geoJsonData, {
+      dataProjection: 'EPSG:4326',
+      featureProjection: 'EPSG:3857'
+    }) as unknown) as Feature<Geometry>[]; // Type assertion
+
+    let layer = this[layerProperty];
+    if (layer) {
+      // Update existing layer
+      layer.getSource()?.clear();
+      layer.getSource()?.addFeatures(features);
+    } else {
+      // Create new layer
+      layer = new VectorLayer({
+        source: new VectorSource({ features }),
+      });
+      this[layerProperty] = layer;
+      this.map.addLayer(layer);
+    }
+  }
+
+  // Load Germany GeoJSON data
+  loadGermanyGeoJson(): void {
+    this.http.get('assets/germany.geojson').subscribe( // Load GeoJSON data from assets folder
+      geoJsonData => this.createOrUpdateLayer(geoJsonData, 'germanyGeoJsonLayer'),
+      error => console.error('Error loading Germany GeoJSON:', error)
+    );
+  }
+
+  // Load India GeoJSON data
+  loadIndiaGeoJson(): void {
+    this.http.get('assets/india.geojson').subscribe( // Load GeoJSON data from assets folder
+      geoJsonData => this.createOrUpdateLayer(geoJsonData, 'indiaGeoJsonLayer'),
+      error => console.error('Error loading India GeoJSON:', error)
+    );
+  }
+
+  // Add draw interaction
+  addDrawInteraction(drawType: 'Point' | 'LineString' | 'Polygon' | 'Circle'): void {
+    let style;
+    switch (drawType) {
+      case 'Point':
+        style = this.pointStyle;
+        break;
+      case 'LineString':
+        style = this.lineStyle;
+        break;
+      case 'Polygon':
+        style = this.polygonStyle;
+        break;
+      case 'Circle':
+        style = this.circleStyle;
+        break;
+    }
+    // Add draw interaction only when drawing is enabled
+    if (this.isDrawing && this.drawingLayer && this.drawingLayer.getSource()) { 
+      this.removeDrawInteractions();
+
+      const draw = new Draw({
+        source: this.drawingLayer.getSource() as VectorSource<Feature<Geometry>>,
+        type: drawType,
+        style: style
+      });
+      this.map.addInteraction(draw);
+    }
+  }
+
+  // Draw point
+  startDrawingPoint(): void {
+    this.addDrawInteraction('Point');
+  }
+
+  // Draw line
+  startDrawingLine(): void {
+    this.addDrawInteraction('LineString');
+  }
+
+  // Draw polygon
+  startDrawingPolygon(): void {
+    this.addDrawInteraction('Polygon');
+  }
+
+  // Draw circle
+  startDrawingCircle(): void {
+    this.addDrawInteraction('Circle');
+  }
+
+  // Toggle drawing
+  toggleDrawing(): void {
+    this.isDrawing = !this.isDrawing;
+    // Add draw interaction and remove map click listener only when drawing is enabled
+    if (this.isDrawing) { 
+      this.map.un('singleclick', this.addMapClickListener);                                  
+      this.addDrawInteraction('Point');
+    } else {
+      this.removeDrawInteractions();
+      this.addMapClickListener();
+    }
+  }
+
+  // Remove draw interactions
+  removeDrawInteractions(): void {
+    const drawInteractions = this.map.getInteractions().getArray()
+      .filter(interaction => interaction instanceof Draw);
+    drawInteractions.forEach(interaction => this.map.removeInteraction(interaction));
+  }
+
+  // Reload page
+  reloadPage(): void {
+    window.location.reload();
   }
 }
